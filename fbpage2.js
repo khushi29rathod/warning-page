353ab2e43a30d13bfc43c22f7840656e47ca8708
khushi <you@example.com>
2022-12-02 17:24:24 +0530

    first commit

---
+++ fbpage2.js
@@ -0,0 +1,12 @@
+
+var btn=document.querySelector("#warning");
+var a=document.querySelector("#box3");
+var btn1=document.querySelector("#close");
+btn.addEventListener('click',function(){
+a.style.display="initial";
+
+})
+btn1.addEventListener('click',function(){
+    a.style.display="none";                                                                                     
+    
+    })
